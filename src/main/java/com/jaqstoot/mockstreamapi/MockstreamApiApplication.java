66fae18f399995dd39dc5a8b5efafc36da0f290f
JaqStoot <jtstout@protonmail.com>
2026-02-18 21:46:23 -0600

    Initial repo commit

---
+++ src/main/java/com/jaqstoot/mockstreamapi/MockstreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.jaqstoot.mockstreamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockstreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockstreamApiApplication.class, args);
+	}
+
+}
